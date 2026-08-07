0cec6d77c4ce793c0117342fd33ec4bee24d846b
kashish0903 <tomshreepali@gmail.com>
2026-08-07 16:37:31 +0545

    add 3-1.py

---
+++ 3-1.py
@@ -0,0 +1,23 @@
+#3-1:
+names = ["kripesh","roshan","anjali","dipesh","parbat"]
+print(names[0])
+print(names[1])
+print(names[2])
+print(names[3])
+print(names[4])
+print()
+#3-2:
+name = ["kripesh","roshan","anjali","dipesh","parbat"]
+print(f"Hello {names[0]}, how are you?")
+print(f"Hello {names[1]}, how are you?")
+print(f"Hello {names[2]}, how are you?")
+print(f"Hello {names[3]}, how are you?")
+print(f"Hello {names[4]}, how are you?")
+print()
+#3-3
+car = ["mercedes","BMW","G-Wagon","Tesla","Porsche"]
+print(f"I would like to own a {car[0]}.")
+print(f"I would like to own a {car[1]}.")
+print(f"I would like to own a {car[2]}.")
+print(f"I would like to own a {car[3]}.")
+print(f"I would like to own a {car[4]}.")
