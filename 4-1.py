b6e2c443be59e211e4d08d143541507f09642852
kashish0903 <tomshreepali@gmail.com>
2026-08-15 21:13:06 +0545

    add python

---
+++ 4-1.py
@@ -0,0 +1,15 @@
+# ===== 4-1 =====
+pizzas = ["New York Style", "Neapolitan", "Deep Dish"]
+
+for pizza in pizzas:
+    print(f"I like {pizza} pizza.")
+
+print("I really love pizza!")
+
+# ===== 4-2 =====
+animals = ["Cat", "Dog", "Cow"]
+
+for animal in animals:
+    print(f"A {animal} would make a great pet.")
+
+print("Any of these animals would make a great pet!")
