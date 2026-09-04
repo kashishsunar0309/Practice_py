0e7e4933d4a8835dde22510bb162fee825e1c1b8
kashish0903 <tomshreepali@gmail.com>
2026-09-04 13:09:09 +0545

    add tuple concept

---
+++ 4-13.py
@@ -0,0 +1,16 @@
+simple_foods = ("chicken-rice","noddles-egg","roti-aaulu","milk","rice_dal")
+for simple_food in simple_foods:
+    print(simple_food)
+#Reject on 
+"""
+simple_foods[0] = "burger"
+print(simple_foods[0])
+"""
+#Restaurant change
+print("Original: \n")
+for x in simple_foods:
+    print(x)
+simple_foods = ("chicken-rice","noddles-egg","veg-rice","milk","salat")
+print("Modiflied: \n")
+for y in simple_foods:
+    print(y)
